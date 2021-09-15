cc6e6dc907b09eab3e52bcf2ee3f3f7692810a95
simonpuno <simonpuno@gmail.com>
2021-09-15 07:48:54 -0700

    Add number labels on map markers

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -5,19 +5,37 @@ export default class MarkerManager {
     }
     
     updateMarkers(businesses) {
-        businesses.forEach(business => {
-            if (!Object.keys(this.markers).includes(business.id.toString())) {
-                this.createMarkerFromBusiness(business)
-            }
-        })
+        if (businesses.length > 1) {
+            businesses.forEach((business, i) => {
+                if (!Object.keys(this.markers).includes(business.id.toString())) {
+                    this.createMarkerFromBusiness(business, i)
+                }
+            })
+        } else if (businesses.length === 1) {
+            businesses.forEach((business) => {
+                if (!Object.keys(this.markers).includes(business.id.toString())) {
+                    this.createMarkerFromBusiness(business, 'single')
+                }
+            })
+        }
     }
 
-    createMarkerFromBusiness(business) {
-        const latLng = { lat: business.lat, lng: business.lng };
-        this.markers[business.id] = business;
-        new google.maps.Marker({
-            position: latLng,
-            map: this.map
-        })
+    createMarkerFromBusiness(business, i) {
+        if (i === 'single') {
+            const latLng = { lat: business.lat, lng: business.lng };
+            this.markers[business.id] = business;
+            new google.maps.Marker({
+                position: latLng,
+                map: this.map,
+            })
+        } else {
+            const latLng = { lat: business.lat, lng: business.lng };
+            this.markers[business.id] = business;
+            new google.maps.Marker({
+                position: latLng,
+                map: this.map,
+                label: `${i+1}`
+            })
+        }
     }
 }
